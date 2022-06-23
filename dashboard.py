5593af17684b51ce574e92c75e73dba6d4278bd4
pacodev0312 <pacodev0312@gmail.com>
2022-06-23 13:35:54 -0300

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,9 +1,7 @@
 # Libraries
 import streamlit as st
 import pandas as pd
-git clone https://github.com/geopandas/geopandas.git
-cd geopandas
-pip install .
+pip install git+git://github.com/geopandas/geopandas.git
 import geopandas as gpd
 from geopandas import GeoDataFrame
 import matplotlib.pyplot as plt
