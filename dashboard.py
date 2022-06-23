6b132a77d983a02f481e7c12eb42a65e6fa1b8a9
pacodev0312 <pacodev0312@gmail.com>
2022-06-23 13:33:06 -0300

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,7 +1,7 @@
 # Libraries
 import streamlit as st
 import pandas as pd
-!pip install geopandas
+pip install geopandas
 import geopandas as gpd
 from geopandas import GeoDataFrame
 import matplotlib.pyplot as plt
