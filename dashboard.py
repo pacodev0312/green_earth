d9f268f1b233c25551bbddd82159a79f1381be2d
pacodev0312 <pacodev0312@gmail.com>
2022-06-25 16:19:49 -0300

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -120,18 +120,19 @@ drivers = data_drivers.loc[data_drivers['year'] == year]
 
 #area_drivers_total = drivers['area_ha'].sum()
 commodities = drivers.loc[drivers['dominant_driver'] == 1,'area_ha']
-itinerant = drivers.loc[drivers['dominant_driver'] == 2,'area_ha']
-floresta = drivers.loc[drivers['dominant_driver'] == 3,'area_ha']
-queimadas = drivers.loc[drivers['dominant_driver'] == 4,'area_ha']
-urbanizacao = drivers.loc[drivers['dominant_driver'] == 5,'area_ha']
+st.write(commodities)
+#itinerant = drivers.loc[drivers['dominant_driver'] == 2,'area_ha']
+#floresta = drivers.loc[drivers['dominant_driver'] == 3,'area_ha']
+#queimadas = drivers.loc[drivers['dominant_driver'] == 4,'area_ha']
+#urbanizacao = drivers.loc[drivers['dominant_driver'] == 5,'area_ha']
 
-labels=['Commodities','Agricultura Itinerante','Floresta','Queimadas','Urbanização']
-sizes = [commodities,itinerant,floresta,queimadas,urbanizacao]
+#labels=['Commodities','Agricultura Itinerante','Floresta','Queimadas','Urbanização']
+#sizes = [commodities,itinerant,floresta,queimadas,urbanizacao]
 
-fig5, ax5 = plt.subplots()
-ax5.pie(sizes, labels=labels, autopct='%1.1f%%',shadow=False, startangle=90,colors='PuRd')
-ax5.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-st.pyplot(fig5)
+#fig5, ax5 = plt.subplots()
+#ax5.pie(sizes, labels=labels, autopct='%1.1f%%',shadow=False, startangle=90,colors='PuRd')
+#ax5.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+#st.pyplot(fig5)
 
 #labels = data_drivers['year'].unique().tolist()
 #commodities = 
