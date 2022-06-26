5530d0ff4044c4df068a5ace7d5503fa79ecd5d8
pacodev0312 <pacodev0312@gmail.com>
2022-06-25 21:46:34 -0300

    teste

--- dashboard.py
+++ dashboard.py
@@ -137,9 +137,9 @@ with col1_2:
   with st.expander ('Principais Causas de Perda Florestal') :
     st.text(year)
     st.write(fig5)
-    st.write('1 - Commodities             4 - Queimadas')
-    st.write('2 - Agricultura Itinerante  5 - Urbanização ')
-    st.write('3 - Florestas')
+    st.text('1 - Commodities             4 - Queimadas')
+    st.text('2 - Agricultura Itinerante  5 - Urbanização ')
+    st.text('3 - Florestas')
 
 with col2_1:
   with st.expander ('Dados:') :
